af42de022650e5fddceff1a03e11b969547106f0
Feng Yukun <fengyukunfyk@gmail.com>
2026-01-21 19:13:15 +0800

    设置QQ允许Null，解决潜在migrate问题

--- backend/xmmcg/users/models.py
+++ backend/xmmcg/users/models.py
@@ -7,7 +7,7 @@ from xmmcg.settings import DEFAULT_USER_TOKENS
 class UserProfile(models.Model):
     """用户扩展信息模型"""
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    qqid = models.CharField(max_length=20, unique=True, null=False, blank=True, help_text='用户QQ号')
+    qqid = models.CharField(max_length=20, unique=True, null=True, blank=True, help_text='用户QQ号')
     token = models.IntegerField(default=DEFAULT_USER_TOKENS, help_text='用户虚拟货币余额')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
