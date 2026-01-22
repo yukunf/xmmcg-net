9b53e92a2e1bed8c9620d97b5de31bd7aa873919
Feng Yukun <fengyukunfyk@gmail.com>
2026-01-22 19:39:13 +0800

    草泥马自动补全

--- backend/xmmcg/xmmcg/urls.py
+++ backend/xmmcg/xmmcg/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from backend.xmmcg.xmmcg.settings import REST_FRAMEWORK
+from settings import REST_FRAMEWORK
 
 urlpatterns = [
     path("admin/", admin.site.urls),
